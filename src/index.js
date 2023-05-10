669d96b422704996f7cc3b2bb5a88c59d4d9d647
jobil <j.bilongo@ynote.africa>
2023-05-10 17:46:55 +0100

    add translation for insuree

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import messages_fr_mr from "./translations/fr_MR.json";
-//import messages_fr from "./translations/fr.json";
+import messages_fr from "./translations/fr.json";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: 'fr_mr', messages: messages_fr_mr }],
+  "translations": [{ key: 'fr_mr', messages: messages_fr_mr }, { key: 'fr', messages: messages_fr }],
 }
 
 export const LanguageFrMrModule = (cfg) => {
